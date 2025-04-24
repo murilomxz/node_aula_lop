31434aa78467dc5eabd491d938321e4932abc710
MURILO MACHADO BRANDALEZI <25170098@educ123.sp.senai.br>
2025-04-24 10:02:02 -0300

    altercao arquvio

--- funções/Moldura.js
+++ funções/Moldura.js
@@ -1,10 +1,10 @@
-
+//desenha uma moldura em frase
 
 const readline= require("readline-sync");
 const frase=readline.question('Digite uma frase: ');
 const cr= '█';
 for(let i = 0; i<frase.length+2; i++){
-    process.stdout.write(cr); //desenha uma moldura em frase
+    process.stdout.write(cr); 
     
 }
 console.log(`\n${cr}${frase}${cr}`)
