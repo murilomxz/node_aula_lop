5a196ec2d3a188c0e321beb0a1157e7b1e2b2ece
MURILO MACHADO BRANDALEZI <25170098@educ123.sp.senai.br>
2025-04-30 11:25:02 -0300

    new file

--- agena.js
+++ agena.js
@@ -1,3 +1,11 @@
 const readline = require('readline-sync');
+function menu() {
+    console.log("Projeto Agenda\n")
 
-const opcao=Number(readline.question("\ 1. adicionar \n 2. listar\n 3. buscar usuario \n 4. apagar registro \n 5. salvar em disco \n opcao: "))
+    const opcao = Number(readline.question("\ 1. adicionar \n 2. listar\n 3. buscar usuario \n 4. apagar registro \n 5. salvar em disco \n opcao: "))
+}
+
+function adicionar(){
+
+    
+}
