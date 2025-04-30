a796459737256400c4ab5001e5f125277e21f30b
MURILO MACHADO BRANDALEZI <25170098@educ123.sp.senai.br>
2025-04-30 11:17:33 -0300

    new file

---
+++ agena.js
@@ -0,0 +1,3 @@
+const readline = require('readline-sync');
+
+const opcao=Number(readline.question("\ 1. adicionar \n 2. listar\n 3. buscar usuario \n 4. apagar registro \n 5. salvar em disco \n opcao: "))
